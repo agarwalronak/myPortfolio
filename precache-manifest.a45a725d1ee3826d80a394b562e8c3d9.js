a3be3781b0fc63dd64de6f09e85b2514abf561ef
Ronak <ronakagarwal2042@gmail.com>
2020-05-29 16:06:18 +0530

    Updates

---
+++ precache-manifest.a45a725d1ee3826d80a394b562e8c3d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb50a1013b4769e93c6cccc5f791bd1",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "e9cb7c62dd9c5a06a979",
+    "url": "/myportfolio/static/css/main.784ba14e.chunk.css"
+  },
+  {
+    "revision": "a99e59c1aa72dc03b6cb",
+    "url": "/myportfolio/static/js/2.16e13f82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myportfolio/static/js/2.16e13f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9cb7c62dd9c5a06a979",
+    "url": "/myportfolio/static/js/main.d7b4b21e.chunk.js"
+  },
+  {
+    "revision": "dfcaa98cefeb0fdc319c",
+    "url": "/myportfolio/static/js/runtime-main.6ecf7dfe.js"
+  },
+  {
+    "revision": "61d6683034fe05482213d0465e7efb95",
+    "url": "/myportfolio/static/media/26733485_1576927955733309_6209395833692517777_n.61d66830.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e21b0b3ca287eb03b97dc5f255a06921.js"
+  "/myportfolio/precache-manifest.a45a725d1ee3826d80a394b562e8c3d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
