8fc053e2c18ce487625e9819e89e850fde661445
Ronak <ronakagarwal2042@gmail.com>
2020-05-29 15:34:52 +0530

    Updates

---
+++ precache-manifest.e21b0b3ca287eb03b97dc5f255a06921.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8172780a0764da68d68540b3cbeb28c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "97fa06e60cccbefa26fb",
+    "url": "/portfolio/static/css/main.784ba14e.chunk.css"
+  },
+  {
+    "revision": "a99e59c1aa72dc03b6cb",
+    "url": "/portfolio/static/js/2.16e13f82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.16e13f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97fa06e60cccbefa26fb",
+    "url": "/portfolio/static/js/main.69715987.chunk.js"
+  },
+  {
+    "revision": "83f657771b7854dba4c6",
+    "url": "/portfolio/static/js/runtime-main.911f39c4.js"
+  },
+  {
+    "revision": "61d6683034fe05482213d0465e7efb95",
+    "url": "/portfolio/static/media/26733485_1576927955733309_6209395833692517777_n.61d66830.jpg"
+  }
+]);
