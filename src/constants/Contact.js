b8816fbe8344cec30d0a3b2dd15103cf0b16f82c
Ronak <ronakagarwal2042@gmail.com>
2020-05-30 03:49:06 +0530

    Update details - 1

--- src/constants/Contact.js
+++ src/constants/Contact.js
@@ -8,9 +8,6 @@ export const contacts = {
             name: "Mobile Number",
             details: "9748074394"
         }, {
-            name: "Location",
-            details: "103, Girish Ghosh Road, Belurmath,\nSitala Apartment Flat - 203 \nHowrah - 711202, West Bengal, India"
-        }, {
             name: "LinkedIn",
             details: "https://www.linkedin.com/in/ronak-agarwal-91181a169"
         }
